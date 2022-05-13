f90b7a16144ec92d2a0688e545d3e6248901f434
Colin Edwards <ColinJhEdwards@gmail.com>
2022-05-12 19:56:18 -0700

    working with props and functions in react+typescript

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,15 +1,22 @@
 import React, { useRef } from "react";
 
-const NewTodo: React.FC = () => {
-  const ref = useRef();
+type NewTodoProps = {
+  onAddTodo: (todoText: string) => void;
+};
+
+const NewTodo: React.FC<NewTodoProps> = ({ onAddTodo }) => {
+  // since the useRef hook is pointing to a input element, we will indicate that with <>
+  const textInputRef = useRef<HTMLInputElement>(null);
   const todoSubmitHandler = (e: React.FormEvent) => {
     e.preventDefault();
+    const enteredText = textInputRef.current!.value;
+    onAddTodo(enteredText);
   };
   return (
     <form onSubmit={todoSubmitHandler}>
       <div>
         <label htmlFor="todo-text">Todo Text</label>
-        <input type="text" id="todo-text" />
+        <input type="text" id="todo-text" ref={textInputRef} />
       </div>
       <button type="submit">ADD TODO</button>
     </form>
